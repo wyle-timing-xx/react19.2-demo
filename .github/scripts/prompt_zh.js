03b220d2fde545276286f94e8912eb0a22be3a23
rexingleung <rexingleung@126.com>
2025-11-11 06:22:09 +0800

    added prompt zh

--- .github/scripts/prompt_zh.js
+++ .github/scripts/prompt_zh.js
@@ -1,5 +1,5 @@
 import fs from "fs";
 
-const promptZh = fs.readFileSync("./.github/scripts/prompt_zh.md", "utf-8");
+const promptZh = fs.readFileSync(".github/scripts/prompt_zh.md", "utf-8");
 
 export default promptZh.toString();
