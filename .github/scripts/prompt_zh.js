ea4fbe835d7d28ce1d7b2df4c80ba24a1c5e564a
rexingleung <rexingleung@126.com>
2025-11-11 06:31:06 +0800

    added prompt zh

--- .github/scripts/prompt_zh.js
+++ .github/scripts/prompt_zh.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from 'path';
 
 // 读取提示词
-const promptPath = path.resolve('.github/prompts/prompt_zh.txt');
+const promptPath = path.resolve('.github/scripts/prompt_zh.txt');
 const promptZh = fs.readFileSync(promptPath, "utf-8");
 
 export default promptZh.toString();
